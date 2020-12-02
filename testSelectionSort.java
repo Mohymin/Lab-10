64d993aaceb69c3c030f9c392c0e2ba2171ca2d0
Mohymin Islam <mohymin@umich.edu>
2020-12-02 00:16:55 -0500

    Function added

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -8,9 +8,26 @@ class testSelectionSort {
 
 	@Test
 	void test() {
+		testPositive();
+		testNegatives();
 
 	}
 	SelectionSort sort = new SelectionSort();
 
+	public void testPositive(){
+	        int[] arr = new int[]{8,9,7,10,2};
+	        int[] res = new int[]{2,7,8,9,10};
+	        sort.basicSelectionSort(arr);
+	        assert(Arrays.equals(arr,res));
+	    }
+
+	    @Test
+	 public void testNegatives(){
+	        int[] arr = new int[]{-8,-9,-7,-10,-2};
+	        int[] res = new int[]{-10,-9,-8,-7,-2};
+	        sort.basicSelectionSort(arr);
+	        assert(Arrays.equals(arr,res));
+	    }
+			
 
 }
