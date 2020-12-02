c33e5caed79e5c3150ee0ad17f37c237a5fc2ff5
Mohymin Islam <mohymin@umich.edu>
2020-12-02 00:07:33 -0500

    init

---
+++ testSelectionSort.java
@@ -0,0 +1,17 @@
+package lab10;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+class testSelectionSort {
+
+	@Test
+	void test() {
+
+	}
+	SelectionSort sort = new SelectionSort();
+
+
+
+}
