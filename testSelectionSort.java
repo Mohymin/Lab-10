0f0abd4773b71b8a174d3180a0666ccbe7373502
Mohymin Islam <mohymin@umich.edu>
2020-12-02 00:24:31 -0500

    Branch2 function added

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -8,9 +8,25 @@ class testSelectionSort {
 
 	@Test
 	void test() {
+		testMixed();
+	  testDuplicates();
 
 	}
 	SelectionSort sort = new SelectionSort();
+	@Test
+     public void testMixed(){
+         int[] arr = new int[]{-8,-9,7,-10,2,0};
+         int[] res = new int[]{-10,-9,-8,0,2,7};
+         sort.basicSelectionSort(arr);
+         assert(Arrays.equals(arr,res));
+     }
 
+     @Test
+     public void testDuplicates(){
+         int[] arr = new int[]{-8,-9,7,2,-8,2,-10,2,0};
+         int[] res = new int[]{-10,-9,-8,-8,0,2,2,2,7};
+         sort.basicSelectionSort(arr);
+         assert(Arrays.equals(arr,res));
+     }
 
 }
